e49de920af41c3f79ee486cd242bd46036e6810b
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-07 20:03:02 -0600

    Create folders for models and routes for future implementation

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./startup/db');
-// const temps = require('./routes/temps');
-// const volumes = require('./routes/volumes');
-
-
+const students = require('./routes/students');
+const tests = require('./routes/tests');
 
 const app = express();
 
@@ -12,8 +10,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-// app.use('/api/temp', temps);
-// app.use('/api/volume', volumes);
+app.use('/api/students', students);
+app.use('/api/tests', tests);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
