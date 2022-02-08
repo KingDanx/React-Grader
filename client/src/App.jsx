8ed1d0ddb8348948dedee1cef37175e3237aa501
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-07 18:41:27 -0600

    Set up React.js

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  return ( 
+    <div>
+      Test React
+    </div>
+   );
+}
+ 
+export default App;
