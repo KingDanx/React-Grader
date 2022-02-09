ca53b1abf2a346806f1b48d07ca3a87a081cf272
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-08 19:37:17 -0600

    Conditional rendering to prevent invalid inputs crossing temperature with volume

--- client/src/components/pages/CreateTest/CreateTest.jsx
+++ client/src/components/pages/CreateTest/CreateTest.jsx
@@ -33,47 +33,110 @@ const CreateTask = () => {
         {mapArray.map((el, index) => {
           return (
             <div key={index}>
-              <TextField
-                sx={{ m: 1, maxWidth: 120 }}
-                id="test-input"
-                label="Number"
-                name={`inputNumber${index}`}
-                type="text"
-                onChange={(event) => handleChange(event)}
-                error={isNaN(formValue[`inputNumber${index}`]) ? true : false}
-                helperText={
-                  isNaN(formValue[`inputNumber${index}`]) ? "Input a Number" : null
-                }
-              />
-              <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel htmlFor="grouped-native-select">
-                  Input Unit
-                </InputLabel>
-                <Select
-                  native
-                  defaultValue=""
-                  id="unit-select"
-                  label="Units"
-                  name={`inputUnit${index}`}
+              <div>
+                <TextField
+                  sx={{ m: 1, maxWidth: 120 }}
+                  id="test-input"
+                  label="Number"
+                  name={`inputNumber${index}`}
+                  type="text"
                   onChange={(event) => handleChange(event)}
-                >
-                  <option aria-label="None" value="" />
-                  <optgroup label="Temperature">
-                    <option value={"kelvin"}>Kelvin</option>
-                    <option value={"celsius"}>Celsius</option>
-                    <option value={"fahrenheit"}>Fahrenheit</option>
-                    <option value={"rankine"}>Rankine</option>
-                  </optgroup>
-                  <optgroup label="Volume">
-                    <option value={"liters"}>Liters</option>
-                    <option value={"tablespoons"}>Tablespoons</option>
-                    <option value={"cubicInches"}>Cubic-Inches</option>
-                    <option value={"cubicFeet"}>Cubic-Feet</option>
-                    <option value={"cups"}>Cups</option>
-                    <option value={"gallons"}>Gallons</option>
-                  </optgroup>
-                </Select>
-              </FormControl>
+                  error={isNaN(formValue[`inputNumber${index}`]) ? true : false}
+                  helperText={
+                    isNaN(formValue[`inputNumber${index}`])
+                      ? "Input a Number"
+                      : null
+                  }
+                />
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel htmlFor="grouped-native-select">
+                    Input Unit
+                  </InputLabel>
+                  <Select
+                    native
+                    defaultValue=""
+                    id="unit-select"
+                    label="Units"
+                    name={`inputUnit${index}`}
+                    onChange={(event) => handleChange(event)}
+                  >
+                    <option aria-label="None" value="" />
+                    <optgroup label="Temperature">
+                      <option value={"kelvin"}>Kelvin</option>
+                      <option value={"celsius"}>Celsius</option>
+                      <option value={"fahrenheit"}>Fahrenheit</option>
+                      <option value={"rankine"}>Rankine</option>
+                    </optgroup>
+                    <optgroup label="Volume">
+                      <option value={"liters"}>Liters</option>
+                      <option value={"tablespoons"}>Tablespoons</option>
+                      <option value={"cubicInches"}>Cubic-Inches</option>
+                      <option value={"cubicFeet"}>Cubic-Feet</option>
+                      <option value={"cups"}>Cups</option>
+                      <option value={"gallons"}>Gallons</option>
+                    </optgroup>
+                  </Select>
+                </FormControl>
+              </div>
+              <div>
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel htmlFor="grouped-native-select">
+                    Output Unit
+                  </InputLabel>
+                  <Select
+                    native
+                    defaultValue=""
+                    id="unit-select"
+                    label="Units"
+                    name={`outputUnit${index}`}
+                    onChange={(event) => handleChange(event)}
+                  >
+                    <option aria-label="None" value="" />
+                    {formValue[`inputUnit${index}`] === `kelvin` ||
+                    formValue[`inputUnit${index}`] === `celsius` ||
+                    formValue[`inputUnit${index}`] === `fahrenheit` ||
+                    formValue[`inputUnit${index}`] === `rankine` ? (
+                      <optgroup label="Temperature">
+                        <option value={"kelvin"}>Kelvin</option>
+                        <option value={"celsius"}>Celsius</option>
+                        <option value={"fahrenheit"}>Fahrenheit</option>
+                        <option value={""}>Rankine</option>
+                      </optgroup>
+                    ) : null}
+                    {formValue[`inputUnit${index}`] === `liters` ||
+                    formValue[`inputUnit${index}`] === `tablespoons` ||
+                    formValue[`inputUnit${index}`] === `cubicInches` ||
+                    formValue[`inputUnit${index}`] === `cubicFeet` ||
+                    formValue[`inputUnit${index}`] === `cups` ||
+                    formValue[`inputUnit${index}`] === `gallons` ? (
+                      <optgroup label="Volume">
+                        <option value={"liters"}>Liters</option>
+                        <option value={"tablespoons"}>Tablespoons</option>
+                        <option value={"cubicInches"}>Cubic-Inches</option>
+                        <option value={"cubicFeet"}>Cubic-Feet</option>
+                        <option value={"cups"}>Cups</option>
+                        <option value={"gallons"}>Gallons</option>
+                      </optgroup>
+                    ) : null}
+                  </Select>
+                </FormControl>
+                <TextField
+                  sx={{ m: 1, maxWidth: 120 }}
+                  id="test-input"
+                  label="Number"
+                  name={`outputNumber${index}`}
+                  type="text"
+                  onChange={(event) => handleChange(event)}
+                  error={
+                    isNaN(formValue[`outputNumber${index}`]) ? true : false
+                  }
+                  helperText={
+                    isNaN(formValue[`outputNumber${index}`])
+                      ? "Input a Number"
+                      : null
+                  }
+                />
+              </div>
             </div>
           );
         })}
