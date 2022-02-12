a1cd145a4a5103bb80c9b443176fe545be7c86bd
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-12 00:56:44 -0600

    Users can no longer input invalid data into number fields before subbmiting a form and all form data has error text if not filled properly

--- client/src/components/pages/GradeTest/components/TestGrader.jsx
+++ client/src/components/pages/GradeTest/components/TestGrader.jsx
@@ -5,59 +5,88 @@ import axios from "axios";
 import Button from "@mui/material/Button";
 
 const TestGrader = ({ test, setTest }) => {
-  
-    const userOutput = [];
+  const userOutput = [];
 
-    const gradeTest = async () => {
+  const gradeTest = async () => {
     await axios
-    .put(`http://localhost:5000/api/tests/gradeTest/${test._id}`, {
-        outputNumber: test.outputNumber.map((el, i) => formValue[`question${i}`])
-    })
-    .then((res) => {
-      setTest(res.data);
-      console.log(res);
-    })
-    .catch(function (error) {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    });
-};
+      .put(`http://localhost:5000/api/tests/gradeTest/${test._id}`, {
+        outputNumber: test.outputNumber.map(
+          (el, i) => formValue[`question${i}`]
+        ),
+      })
+      .then((res) => {
+        setTest(res.data);
+        console.log(res);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  };
 
-  const { formValue, handleChange, handleSubmit, setFormValue } = useForm(gradeTest);
-  
+  const { formValue, handleChange, handleSubmit, setFormValue } =
+    useForm(gradeTest);
+
   useEffect(() => {
-    setFormValue('');
+    setFormValue("");
   }, [test]);
 
   return (
     <div>
-      {!test.inputNumber
-        ? <h2>{"Select a test to grade."}</h2>
-        : <h2>{`Grading ${test.testName}`}</h2>}
+      {!test.inputNumber ? (
+        <h2>{"Select a test to grade."}</h2>
+      ) : (
+        <h2>{`Grading ${test.testName}`}</h2>
+      )}
       <div>
         {!test.inputNumber
           ? null
           : test.inputNumber.map((el, i) => (
               <div key={i}>
-                Convert {el} <b>{test.inputUnit[i]}</b> to <b>{test.outputUnit[i]}</b>
+                Convert {el} <b>{test.inputUnit[i]}</b> to{" "}
+                <b>{test.outputUnit[i]}</b>
                 <TextField
                   onChange={(event) => handleChange(event)}
                   size="small"
                   name={`question${i}`}
-                  value={!formValue[`question${i}`] ? "" : formValue[`question${i}`]}/>
-                {test.correct[i] === true ? "Correct" : "Incorrect"}  
+                  value={
+                    !formValue[`question${i}`] ? "" : formValue[`question${i}`]
+                  }
+                  error={isNaN(formValue[`question${i}`]) ? true : false}
+                  helperText={
+                    isNaN(formValue[`question${i}`]) ? "Input a Number" : null
+                  }
+                />
+                {test.correct[i] === true ? " Correct" : " Incorrect"}
               </div>
             ))}
       </div>
-      {!test.inputNumber ? null : <Button variant="contained" onClick={(event)=>handleSubmit(event)}>Grade Test</Button>}
+      {!test.inputNumber ? null : (
+        <Button
+          disabled={
+            test.inputNumber.some(
+              (so, i) =>
+                formValue[`question${i}`] === "" ||
+                !formValue[`question${i}`] ||
+                isNaN(formValue[`question${i}`])
+            ) === true
+              ? true
+              : false
+          }
+          variant="contained"
+          onClick={(event) => handleSubmit(event)}
+        >
+          Grade Test
+        </Button>
+      )}
     </div>
   );
 };
