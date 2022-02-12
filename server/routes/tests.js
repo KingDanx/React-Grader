7f672b37c77301c510d0d60e2a0650fd822e0441
Dan Prudhomme <dannyprud@yahoo.com>
2022-02-11 18:29:14 -0600

    Backend temperature conversion functional

--- server/routes/tests.js
+++ server/routes/tests.js
@@ -2,6 +2,7 @@ const { Test, validateTest, testSchema } = require("../models/test");
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
+const Temperature = require("temperature-js");
 
 //Create a new Test
 router.post("/createTest", async (req, res) => {
@@ -42,6 +43,14 @@ router.get('/:testId', async (req, res) => {
     return res.status(500).send(`Internal Server Error: ${ex}`);
   }
 });
+
+
+const tempConverstion = (inputUnit, outputUnit, inputNumber) => {
+  let firstLetterUppercase = outputUnit.charAt(0).toUpperCase() + outputUnit.slice(1);
+    return Temperature[`${inputUnit}To${firstLetterUppercase}`](inputNumber);
+}
+
+console.log(tempConverstion(`celcius`, `kelvin`, 10));
 
 router.put('/gradeTest/:testId', async (req, res) => {
   try {
